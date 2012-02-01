c59ca5b52c6259bb0782c1ec95a722135cb340b5
marisol <88marisol@gmail.com>
2012-02-01 01:29:08 -0500

    se termino CRUD para memorandos en el controlador index
    -se agregó metodo listMemo para administrador y demás usuarios
    -se agregó metodo para borrar y modificar memos dentro de el controlador index

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -25,7 +25,6 @@ class IndexController extends Zend_Controller_Action
         $this->auth = Zend_Auth::getInstance();
     }
 
-
     /**
      * \brief Formulario para crear noticia
      *
@@ -91,7 +90,28 @@ class IndexController extends Zend_Controller_Action
 
         $form->addElement('textarea','description',array('label'=>'Description'));
 
-        $form->addElement('submit','create',array('label'=>'Create'));
+        $form->addElement('submit','update',array('label'=>'Update'));
+
+        return $form;
+    }
+
+    /**
+     * \brief Formulario para listar memos
+     *
+     * @return Formulario a action listMemo
+     *
+     */
+
+    public function listMemoForm()
+    {
+        $form=new Zend_Form;
+        $form->setAttrib('class','listmemo');
+        $form->setAction($this->view->url(array("controller" => "index", "action" => "list-memo")))
+        ->setMethod('post');
+
+        $form->addElement('text','user',array('label'=>'Usuario','required'=>true,'filter'=>'StringToLower','validator'=>'StringLength',false,array(6,10),'validator'=>'alnum','validator'=>'regex', false, array('/^[a-z]+/')));
+
+        $form->addElement('submit','Enviar',array('label'=>'Enviar'));
 
         return $form;
     }
@@ -110,6 +130,8 @@ class IndexController extends Zend_Controller_Action
         $form->setAction($this->view->url(array("controller" => "index", "action" => "create-memo")))
         ->setMethod('post');
 
+        $form->addElement('text','user',array('label'=>'Usuario','required'=>true,'filter'=>'StringToLower','validator'=>'StringLength',false,array(6,10),'validator'=>'alnum','validator'=>'regex', false, array('/^[a-z]+/')));
+
         $form->addElement('text','title',array('label'=>'Title','required'=>true,'validator'=>'StringLength',false,array(1,20),'validator'=>'alnum'));
 
         $form->addElement('textarea','description',array('label'=>'Description','required'=>true));
@@ -119,6 +141,28 @@ class IndexController extends Zend_Controller_Action
         return $form;
     }
 
+    /**
+     * \brief Formulario para modificar memos
+     *
+     * @return Formulario a action updateMemo
+     *
+     */
+
+    public function updateMemoForm()
+    {
+        $form=new Zend_Form;
+        $form->setAttrib('class','updatememo');
+        $form->setAction($this->view->url(array("controller" => "index", "action" => "update-memo")))
+        ->setMethod('post');
+
+        $form->addElement('text','title',array('label'=>'Title','validator'=>'StringLength',false,array(1,20),'validator'=>'alnum'));
+
+        $form->addElement('textarea','description',array('label'=>'Description'));
+
+        $form->addElement('submit','update',array('label'=>'Update'));
+
+        return $form;
+    }
 
     /**
      * \brief Formulario para autenticar el usuario.
@@ -187,7 +231,7 @@ class IndexController extends Zend_Controller_Action
             <span>Memorandos</span>
             <ul>
             <a href=".$this->view->url(array('controller'=>'index', 'action'=>'create-memo')).">Crear Memorando</a><br>
-            <a href=".$this->view->url(array('controller'=>'index', 'action'=>'list-memo')).">Editar Memorando</a><br>
+            <a href=".$this->view->url(array('controller'=>'index', 'action'=>'list-memo', 'typememo'=>'edit')).">Editar Memorando</a><br>
             </ul>
             </div>
             </div>";
@@ -302,6 +346,173 @@ class IndexController extends Zend_Controller_Action
     }
 
     /**
+       * \brief action para listar memos
+       *
+       * @return N/A
+       *
+       */
+
+    public function listMemoAction()
+    {
+        $form = $this->ListMemoForm();
+
+        if(!$this->getRequest()->isPost())
+        {
+            echo "<h4 id='infnews'>Usuario Para Listar Memorandos</h4>";
+            echo $form;
+            return;
+        }
+        if(!$form->isValid($this->_getAllParams()))
+        {
+            echo $form;
+            return;
+        }
+
+        $values = $form->getValues();
+
+        $this->view->user = $values['user'];
+
+
+        $iList = $this->getRequest()->getParam('typememo');
+
+        if(!count($this->sql->listMemos($this->view->user)))
+        {
+            echo "<h3>El Usuario No Tiene Memorandos</h3>";
+            return;
+        }
+
+        if (($this->session->type == '1') && ($iList == 'edit'))
+        {
+            $this->view->memos = 1;
+            return;
+        }
+        else
+        {
+
+            $this->view->memos = 0;
+
+            return;
+        }
+    }
+
+    /**
+     * \brief action para crear memo
+     *
+     * @return N/A
+     *
+     */
+
+    public function createMemoAction()
+    {
+        if ((!$this->auth->hasIdentity()) || ($this->session->type != '1'))
+        {
+            $this->_helper->redirector('index', 'index');
+            return;
+        }
+        $form = $this->createMemoForm();
+
+        if(!$this->getRequest()->isPost())
+        {
+            echo "<h4 id='infnews'>Datos De Memorando</h4>";
+            echo $form;
+            return;
+        }
+        if(!$form->isValid($this->_getAllParams()))
+        {
+            echo $form;
+            return;
+        }
+        $values = $form->getValues();
+
+        $this->sql->insertMemo($values['user'],$values['title'],$values['description']);
+
+        $this->_helper->redirector('index', 'index');
+        return;
+    }
+
+    /**
+     * \brief action para modificar memos
+     *
+     * @return N/A
+     *
+     */
+
+    public function updateMemoAction()
+    {
+        if ((!$this->auth->hasIdentity()) || ($this->session->type != '1'))
+        {
+            $this->_helper->redirector('index', 'index');
+            return;
+        }
+        if(!$this->_hasParam('memo') && !$this->_hasParam('cc'))
+        {
+            $this->_helper->redirector('index', 'index');
+            return;
+        }
+
+        $form = $this->updateMemoForm();
+        $iIdMemo = $this->getRequest()->getParam('memo');
+        $datos = $this->sql->listMemos($this->getRequest()->getParam('cc'));
+
+        if(!$this->getRequest()->isPost())
+        {
+            echo "<h4 id='infmemo'>Nuevos Datos De Memorando</h4>";
+
+            foreach($datos as $line)
+            {
+                if($line['id'] == $iIdMemo)
+                {
+                    echo $form->populate($line);
+                }
+            }
+            return;
+        }
+
+        if(!$form->isValid($this->_getAllParams()))
+        {
+            echo $form;
+            return;
+        }
+
+        $values = $form->getValues();
+
+        $this->sql->updateMemo($iIdMemo,$values['title'],$values['description']);
+
+        $this->_helper->redirector('index', 'index');
+        return;
+    }
+
+    /**
+     * \brief action para borrar memo
+     *
+     * @return N/A
+     *
+     */
+
+    public function deleteMemoAction()
+    {
+        if ((!$this->auth->hasIdentity()) || ($this->session->type != '1'))
+        {
+            $this->_helper->redirector('index', 'index');
+            return;
+        }
+
+        if(!$this->_hasParam('memo'))
+        {
+            $this->_helper->redirector('index', 'index');
+            return;
+        }
+
+        $iIdMemo = $this->getRequest()->getParam('memo');
+
+        $this->sql->deleteMemo($iIdMemo);
+
+        $this->_helper->redirector('index', 'index');
+        return;
+    }
+
+
+    /**
        * \brief action para listar noticias
        *
        * @return N/A
@@ -365,40 +576,7 @@ class IndexController extends Zend_Controller_Action
         $this->_helper->redirector('index', 'index');
         return;
     }
-    /**
-     * \brief action para crear memo
-     *
-     * @return N/A
-     *
-     */
 
-    public function createMemoAction()
-    {
-        if ((!$this->auth->hasIdentity()) || ($this->session->type != '1'))
-        {
-            $this->_helper->redirector('index', 'index');
-            return;
-        }
-        $form = $this->createMemoForm();
-
-        if(!$this->getRequest()->isPost())
-        {
-            echo "<h4 id='infnews'>Datos De Memorando</h4>";
-            echo $form;
-            return;
-        }
-        if(!$form->isValid($this->_getAllParams()))
-        {
-            echo $form;
-            return;
-        }
-        $values = $form->getValues();
-
-        $this->sql->insertMemo($this->session->user,$values['title'],$values['description']);
-
-        $this->_helper->redirector('index', 'index');
-        return;
-    }
 
     /**
      * \brief action para modificar noticia
