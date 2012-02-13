c2f8804e1734bbf79d4f752df5511346757ba555
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-02-13 00:32:15 -0500

    Modificamos los mensajes de error. Los ponemos en español.

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -19,13 +19,13 @@ class ErrorController extends Zend_Controller_Action
             // 404 error -- controller or action not found
             $this->getResponse()->setHttpResponseCode(404);
             $priority = Zend_Log::NOTICE;
-            $this->view->message = 'Page not found';
+            $this->view->message = 'Página no encontrada';
             break;
         default:
             // application error
             $this->getResponse()->setHttpResponseCode(500);
             $priority = Zend_Log::CRIT;
-            $this->view->message = 'Application error';
+            $this->view->message = 'Ocurrió un error inesperado';
             break;
         }
 
