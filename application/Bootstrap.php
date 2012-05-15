8e142bca73f166ab84d2f2c77bfd0f49b2e8d4a4
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-05-14 20:52:18 -0500

    Configuramos los modelos.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,21 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    protected function _initAutoloader()
+    {
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->registerNamespace('App_')->setFallbackAutoloader(true);
+ 
+        $resourceAutoloader = new Zend_Loader_Autoloader_Resource(
+            array(
+                'basePath' => APPLICATION_PATH,
+                'namespace' => 'App',
+                'resourceTypes' => array(
+                    'form' => array('path' => 'forms/', 'namespace' => 'Form'),
+                    'model' => array('path' => 'models/', 'namespace' => 'Model')
+                )
+            )
+        );
+    }
 }
 
