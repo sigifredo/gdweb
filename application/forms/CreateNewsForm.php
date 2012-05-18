5f30f0658bd151d95c3bcfa5a816a6715eb455a9
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-05-18 09:31:20 -0500

    Agregamos archivos de los commits anteriores.
    - application/forms/CreateNewsForm.php
    - application/forms/UpdateUserForm.php
    - application/views/scripts/user/list-user.phtml

---
+++ application/forms/CreateNewsForm.php
@@ -0,0 +1,24 @@
+<?php
+
+class CreateNewsForm extends Zend_Form
+{
+
+    public function init()
+    {
+        $this->setAttrib('class','createnews');
+
+        $image = new Zend_Form_Element_File('image');
+        $image->setLabel('Load the image')
+              ->setDestination(APPLICATION_PATH."/../public/img/news/")
+              ->setMaxFileSize(2097152); // limits the filesize on the client side
+        $image->addValidator('Count', false, 1);                // ensure only 1 file
+        $image->addValidator('Size', false, 2097152);            // limit to 2 meg
+        $image->addValidator('Extension', false, 'jpg,jpeg,png,gif');// only JPEG, PNG, and GIFs
+
+        $this->addElement($image);
+        $this->addElement('text','title',array('label'=>'Title','required'=>true,'validator'=>'StringLength',false,array(1,20),'validator'=>'alnum'));
+        $this->addElement('textarea','description',array('label'=>'Description','required'=>true));
+        $this->addElement('submit','create',array('label'=>'Create'));
+    }
+
+}
