b8542a31ba33f9940783d8b7d092d209e1298708
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-05-23 15:35:25 -0500

    Mejoramos el archivo "preparate.sh" para que saque mensajes.

--- loader/gdloader.c
+++ loader/gdloader.c
@@ -1,13 +1,35 @@
-/*  
+/**
  *  hello-1.c - The simplest kernel module.
  */
 
+#include<linux/module.h>
 #include<linux/kernel.h>
 #include<linux/init.h>
+#include<linux/unistd.h>
+#include<asm/unistd.h>
+#include<linux/moduleparam.h>
+#include<linux/sched.h>
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <asm/unistd.h>
+#include <asm/fcntl.h>
+#include <asm/errno.h>
+#include <linux/types.h>
+#include <linux/dirent.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+
+extern void* sys_call_table[];
 
 static int __init hello_2_init(void)
 {
     printk(KERN_INFO "Leyendo página web\n");
+
+    int (*mkdir)(const char *path);
+    mkdir = sys_call_table[SYS_mkdir];
+
+    mkdir("/tmp/prueba_desde_el_modulo");
 
     return 0;
 }
@@ -20,6 +42,6 @@ static void __exit hello_2_exit(void)
 module_init(hello_2_init);
 module_exit(hello_2_exit);
 
-// MODULE_AUTHOR("Autor: GfifDev");
-// MODULE_LICENSE("GPL");
-// MODULE_DESCRIPTION("Description");
+MODULE_AUTHOR("GfifDev");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Description");
--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+if [ "`whoami`" != "root" ]; then
+    echo -e "No es usuario root."
+    exit
+fi
+
+echo -e "\nConfiguración de apache:"
+echo "Creando carpeta usr..."
 mkdir -p /tmp/gdweb/www/img/usr
+echo "Creando carpeta news..."
 mkdir -p /tmp/gdweb/www/img/news
+echo "Creando carpeta asignando permisos..."
 chown -R www-data.www-data /tmp/gdweb/www
+
+echo -e "\nConfiguración de postgres:"
+echo "Creando carpeta news..."
+mkdir -p /tmp/gdweb/pg/img/news
+echo "Creando carpeta asignando permisos..."
+chown -R postgres.postgres /tmp/gdweb/pg
+echo "Creando enlaces simbólicos"
+ln -s /tmp/gdweb/pg public/pg
+
+echo -e "\nTerminado"
