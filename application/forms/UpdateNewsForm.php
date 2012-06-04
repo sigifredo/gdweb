f8d3560bbc08ff7327d8e0daefb3ae68e901ee26
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-06-03 19:13:30 -0500

    Agregamos validadores a los formularios de noticias.

--- application/forms/UpdateNewsForm.php
+++ application/forms/UpdateNewsForm.php
@@ -15,8 +15,8 @@ class UpdateNewsForm extends Zend_Form
         $image->addValidator('Extension', false, 'jpg,jpeg,png,gif');// only JPEG, PNG, and GIFs
 
         $this->addElement($image);
-        $this->addElement('textarea','title',array('label'=>'Título', 'rows'=>1));
-        $this->addElement('textarea','header',array('label'=>'Cabecera', 'rows'=>2));
+        $this->addElement('textarea','title',array('label'=>'Título (*)','required'=>true, 'rows'=>1, 'validators' => array(array('validator' => 'StringLength', 'options' => array(0, 20)))));
+        $this->addElement('textarea','header',array('label'=>'Cabecera (*)','required'=>true, 'rows' => 2, 'validators' => array(array('validator' => 'StringLength', 'options' => array(0, 50)))));
         $this->addElement('textarea','description',array('label'=>'Descripción'));
         $this->addElement('submit','update',array('label'=>'Actualizar'));
     }
