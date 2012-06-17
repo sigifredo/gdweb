0f749da4ef68a7ff943dd02cb58c8297d6175925
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-06-17 12:59:53 -0500

    Terminamos de eliminar del código la clase SQL.php

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -21,21 +21,21 @@ class IndexController extends Zend_Controller_Action
      * @return Formulario a action updateMemo
      *
      */
-    public function updateMemoForm()
-    {
-        $form=new Zend_Form;
-        $form->setAttrib('class','updatememo');
-        $form->setAction($this->view->url(array("controller" => "index", "action" => "update-memo")))
-        ->setMethod('post');
+    // public function updateMemoForm()
+    // {
+    //     $form=new Zend_Form;
+    //     $form->setAttrib('class','updatememo');
+    //     $form->setAction($this->view->url(array("controller" => "index", "action" => "update-memo")))
+    //     ->setMethod('post');
 
-        $form->addElement('text','title',array('label'=>'Title','validator'=>'StringLength',false,array(1,20),'validator'=>'alnum'));
+    //     $form->addElement('text','title',array('label'=>'Title','validator'=>'StringLength',false,array(1,20),'validator'=>'alnum'));
 
-        $form->addElement('textarea','description',array('label'=>'Description'));
+    //     $form->addElement('textarea','description',array('label'=>'Description'));
 
-        $form->addElement('submit','update',array('label'=>'Update'));
+    //     $form->addElement('submit','update',array('label'=>'Update'));
 
-        return $form;
-    }
+    //     return $form;
+    // }
 
     /**
      * \brief action para noticias e ingreso de usuario
@@ -53,102 +53,102 @@ class IndexController extends Zend_Controller_Action
      * \brief action para listar memos
      *
      */
-    public function listMemoAction()
-    {
-        if ((!$this->auth->hasIdentity()) || ($this->session->type == '2'))
-        {
-            $this->_helper->redirector('index', 'index');
-            return;
-        }
+    // public function listMemoAction()
+    // {
+    //     if ((!$this->auth->hasIdentity()) || ($this->session->type == '2'))
+    //     {
+    //         $this->_helper->redirector('index', 'index');
+    //         return;
+    //     }
 
-        $form = $this->ListMemoForm();
+    //     $form = $this->ListMemoForm();
 
-        if(!$this->getRequest()->isPost())
-        {
-            echo "<h4 id='infnews'>Usuario Para Listar Memorandos</h4>";
-            echo $form;
-            return;
-        }
-        if(!$form->isValid($this->_getAllParams()))
-        {
-            echo $form;
-            return;
-        }
+    //     if(!$this->getRequest()->isPost())
+    //     {
+    //         echo "<h4 id='infnews'>Usuario Para Listar Memorandos</h4>";
+    //         echo $form;
+    //         return;
+    //     }
+    //     if(!$form->isValid($this->_getAllParams()))
+    //     {
+    //         echo $form;
+    //         return;
+    //     }
 
-        $values = $form->getValues();
+    //     $values = $form->getValues();
 
-        $this->view->user = $values['user'];
+    //     $this->view->user = $values['user'];
 
 
-        $iList = $this->getRequest()->getParam('typememo');
+    //     $iList = $this->getRequest()->getParam('typememo');
 
-        if(!count($this->sql->listMemos($this->view->user)))
-        {
-            echo "<h3>El Usuario No Tiene Memorandos</h3>";
-            return;
-        }
+    //     if(!count($this->sql->listMemos($this->view->user)))
+    //     {
+    //         echo "<h3>El Usuario No Tiene Memorandos</h3>";
+    //         return;
+    //     }
 
-        if (($this->session->type == '1') && ($iList == 'edit'))
-        {
-            $this->view->memos = 1;
-            return;
-        }
-        else
-        {
-            $this->view->memos = 0;
+    //     if (($this->session->type == '1') && ($iList == 'edit'))
+    //     {
+    //         $this->view->memos = 1;
+    //         return;
+    //     }
+    //     else
+    //     {
+    //         $this->view->memos = 0;
 
-            return;
-        }
-    }
+    //         return;
+    //     }
+    // }
 
     /**
      * \brief action para modificar memos
      *
      */
-    public function updateMemoAction()
-    {
-        if ((!$this->auth->hasIdentity()) || ($this->session->type != '1'))
-        {
-            $this->_helper->redirector('index', 'index');
-            return;
-        }
-        if(!$this->_hasParam('memo') && !$this->_hasParam('cc'))
-        {
-            $this->_helper->redirector('index', 'index');
-            return;
-        }
+    // public function updateMemoAction()
+    // {
+    //     if ((!$this->auth->hasIdentity()) || ($this->session->type != '1'))
+    //     {
+    //         $this->_helper->redirector('index', 'index');
+    //         return;
+    //     }
+    //     if(!$this->_hasParam('memo') && !$this->_hasParam('cc'))
+    //     {
+    //         $this->_helper->redirector('index', 'index');
+    //         return;
+    //     }
 
-        $form = $this->updateMemoForm();
-        $iIdMemo = $this->getRequest()->getParam('memo');
-        $datos = $this->sql->listMemos($this->getRequest()->getParam('cc'));
+    //     $form = $this->updateMemoForm();
+    //     $iIdMemo = $this->getRequest()->getParam('memo');
+    //     $datos = $this->sql->listMemos($this->getRequest()->getParam('cc'));
 
-        if(!$this->getRequest()->isPost())
-        {
-            echo "<h4 id='infmemo'>Nuevos Datos De Memorando</h4>";
+    //     if(!$this->getRequest()->isPost())
+    //     {
+    //         echo "<h4 id='infmemo'>Nuevos Datos De Memorando</h4>";
 
-            foreach($datos as $line)
-            {
-                if($line['id'] == $iIdMemo)
-                {
-                    echo $form->populate($line);
-                }
-            }
-            return;
-        }
+    //         foreach($datos as $line)
+    //         {
+    //             if($line['id'] == $iIdMemo)
+    //             {
+    //                 echo $form->populate($line);
+    //             }
+    //         }
+    //         return;
+    //     }
 
-        if(!$form->isValid($this->_getAllParams()))
-        {
-            echo $form;
-            return;
-        }
+    //     if(!$form->isValid($this->_getAllParams()))
+    //     {
+    //         echo $form;
+    //         return;
+    //     }
 
-        $values = $form->getValues();
+    //     $values = $form->getValues();
 
-        $this->sql->updateMemo($iIdMemo,$values['title'],$values['description']);
+    //     $this->sql->updateMemo($iIdMemo,$values['title'],$values['description']);
 
-        $this->_helper->redirector('index', 'index');
-        return;
-    }
+    //     $this->_helper->redirector('index', 'index');
+    //     return;
+    // }
 
     /**
      * \brief action para borrar memo
@@ -157,27 +157,27 @@ class IndexController extends Zend_Controller_Action
      *
      */
 
-    public function deleteMemoAction()
-    {
-        if ((!$this->auth->hasIdentity()) || ($this->session->type != '1'))
-        {
-            $this->_helper->redirector('index', 'index');
-            return;
-        }
+    // public function deleteMemoAction()
+    // {
+    //     if ((!$this->auth->hasIdentity()) || ($this->session->type != '1'))
+    //     {
+    //         $this->_helper->redirector('index', 'index');
+    //         return;
+    //     }
 
-        if(!$this->_hasParam('memo'))
-        {
-            $this->_helper->redirector('index', 'index');
-            return;
-        }
+    //     if(!$this->_hasParam('memo'))
+    //     {
+    //         $this->_helper->redirector('index', 'index');
+    //         return;
+    //     }
 
-        $iIdMemo = $this->getRequest()->getParam('memo');
+    //     $iIdMemo = $this->getRequest()->getParam('memo');
 
-        $this->sql->deleteMemo($iIdMemo);
+    //     $this->sql->deleteMemo($iIdMemo);
 
-        $this->_helper->redirector('index', 'index');
-        return;
-    }
+    //     $this->_helper->redirector('index', 'index');
+    //     return;
+    // }
 
     public function viewAction()
     {
